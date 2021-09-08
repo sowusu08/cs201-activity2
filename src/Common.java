cffbd18f3fdaeeb472ed53b54e8d9a4eea62115a
Samantha Owusu-Antwi <samantha.owusuantwi@duke.edu>
2021-09-07 23:43:05 -0400

    incorrect implementations of Common.java

--- src/Common.java
+++ src/Common.java
@@ -1,6 +1,12 @@
 import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.*;
+
 public class Common {
-    public int count (String a, String b) {
+    public /*static void*/ int count(String a, String b) {
+        // init num_shared which will be returned
+        int num_shared = 0;
+
         // split each string into array of characters
         char[] a_char = a.toCharArray();
         char[] b_char = b.toCharArray();
@@ -12,7 +18,6 @@ public class Common {
                 a_set.add(i);
             }
         }
-
         HashSet<Character> b_set = new HashSet<>();
         for(char k : b_char){
             if(k != ' ') {
@@ -20,20 +25,33 @@ public class Common {
             }
         }
 
-        // loop through each set and manually find shared characters
-        // init array which will contain shared letters
-        char[] shared = new char[a.length()];
+        // initialize arrayLists for both HashSets
+        ArrayList<Character> a_list = new ArrayList<>();
+        ArrayList<Character> b_list = new ArrayList<>();
 
-        // compare each char in a_set to each char in b_set
-        for(char s : a_set){
-            current_letter = a_set[s];
-
+        // convert each HashSet to ArrayList
+        for(char charA : a_set){
+            a_list.add(charA);
+        }
+        for(char charB : b_set){
+            b_list.add(charB);
         }
 
-        // get intersection of hashsets
-        // return length of intersection
-
-        // convert HashSet to array
-        return 0;
+        // loop through each element in a_set and see if b_set contains it
+        for(char k : a_list){
+            // if both arrayLists contain the element increase count by one
+            if( b_list.contains(k) ){
+                num_shared += 1;
+            }
+        }
+        //System.out.println(num_shared);
+        return num_shared;
     }
+
+    /*public static void main(String[] args){
+        String a_ = "horse";
+        String b_ = "moose";
+        count(a_, b_);
+    }*/
+
 }
